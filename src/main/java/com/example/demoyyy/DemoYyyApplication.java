184d3602c80de5da5c961de38d348347778cf65d
tonywang <349008991@qq.com>
2023-04-02 10:55:29 +0800

    add

---
+++ src/main/java/com/example/demoyyy/DemoYyyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoyyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoYyyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoYyyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoyyy/DemoYyyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoyyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoYyyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
